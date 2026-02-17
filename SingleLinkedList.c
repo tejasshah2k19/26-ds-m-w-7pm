1bd104b6d73277f67ca39147433965bdf3ad6234
Tejas Shah <tejasshah2k19@gmail.com>
2026-02-17 12:25:04 +0530

    single ll menu

--- SingleLinkedList.c
+++ SingleLinkedList.c
@@ -7,34 +7,73 @@ struct node
     struct node *next;
 };
 
+struct node *head = NULL; // implicit
+struct node *tmp, *p;
+
+void addNode(int num)
+{
+    // head
+    if (head == NULL)
+    {
+
+        head = (struct node *)malloc(sizeof(struct node));
+        head->data = num;
+        head->next = NULL;
+    }
+    else
+    {
+        // tmp
+        tmp = (struct node *)malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = NULL;
+        p = head;
+        while (p->next != NULL)
+        {
+            p = p->next;
+        }
+
+        p->next = tmp;
+    }
+}
+
+void display()
+{
+    p = head;
+    while (p != NULL)
+    {
+        printf(" %d", p->data); // 10 20 30 40 50
+        p = p->next;            //
+    }
+}
+
 int main()
 {
 
-    struct node *head = NULL; // implicit
-    struct node *tmp;
+    int choice;
+    int num;
 
-    head = (struct node *)malloc(sizeof(struct node));
-    head->data = 10;
-    head->next = NULL;
+    while (1)
+    {
+        printf("\n0 For Exit\n1 For Add\n2 For List\nEnter choice");
+        scanf("%d", &choice);
 
-    tmp = (struct node *)malloc(sizeof(struct node));
-    tmp->data = 20;
-    tmp->next = NULL;
-    head->next = tmp;
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter numb");
+            scanf("%d", &num);
+            addNode(num);
+            break;
+        case 1 + 1:
+            display();
+            break;
+        case 1 - 1:
+            exit(1);
 
-    tmp = (struct node *)malloc(sizeof(struct node));
-    tmp->data = 30;
-    tmp->next = NULL;
-    head->next->next = tmp;
-
-    tmp = (struct node *)malloc(sizeof(struct node));
-    tmp->data = 40;
-    tmp->next = NULL;
-    head->next->next->next = tmp;
-
-
-    printf("%d %d %d %d",head->data,head->next->data,head->next->next->data,head->next->next->next->data);
-
+        default:
+            break;
+        }
+    }
     return 0;
 }
 
